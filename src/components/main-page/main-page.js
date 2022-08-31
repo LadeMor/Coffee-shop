8672caaff67492b44ba4fe156c6ad34e9de3da8b
LadeMor <sergpyatko@gmail.com>
2022-08-31 12:53:36 +0300

    Updated main page

--- src/components/main-page/main-page.js
+++ src/components/main-page/main-page.js
@@ -4,6 +4,9 @@ import './main-page.scss';
 
 import backgroundimage from "../../img/background-desk.png";
 
+import line from '../../logos/Line.png'
+import coffeebeanstripple from '../../logos/coffee-beans-tripple.png';
+
 class MainPage extends Component{
     render(){
         return(
@@ -13,6 +16,20 @@ class MainPage extends Component{
                 </div>
                 <div className="main-page-text">
                     <h1>Everything You Love About Coffee</h1>
+                    <div className="main-page-text-logo">
+                        <img src={line} alt="line" id="line"/>
+                        <img src={coffeebeanstripple} alt="coffee-beans" id="coffee-beans"/>
+                        <img src={line} alt="line" id="line"/>
+                    </div>
+                    <p>
+                        We makes every day full of energy and taste
+                    </p>
+                    <p>
+                        Want to try our beans?
+                    </p>
+                    <button>
+                        <span className="button-text">More</span>
+                    </button>
                 </div>
             </div>
         );
